85f3a3ffd8ee50d633390a350f34fbb5888a1c9a
조성원 <tjddnjsjo@kakao.com>
2023-12-10 01:01:26 +0900

    fix: TailwindCSS shadcn/ui TypeScript 오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next'
 
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import '@/styles/globals.css'
 
 export const metadata: Metadata = {
   title: {
