b857651ea3ab77a0887af47b4cc93b0960e0898c
조성원 <tjddnjsjo@kakao.com>
2023-12-11 06:58:48 +0900

    feat: 네비게이션바 메뉴 추가

---
+++ src/components/Header/MainNav.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import * as React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { cn } from '@/lib/utils'
+
+export function MainNav() {
+  const pathname = usePathname()
+
+  return (
+    <div className="mr-4 hidden md:flex">
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+        <span className="hidden font-bold sm:inline-block">WONTECH</span>
+      </Link>
+      <nav className="flex items-center space-x-6 text-sm font-medium">
+        <Link
+          href="/article"
+          className={cn(
+            'transition-colors hover:text-foreground/80',
+            pathname?.startsWith('/article')
+              ? 'text-foreground'
+              : 'text-foreground/60',
+          )}
+        >
+          블로그
+        </Link>
+        <Link
+          href="/project"
+          className={cn(
+            'transition-colors hover:text-foreground/80',
+            pathname?.startsWith('/project')
+              ? 'text-foreground'
+              : 'text-foreground/60',
+          )}
+        >
+          프로젝트
+        </Link>
+      </nav>
+    </div>
+  )
+}
