f6966796ef90bbdef05883b654e1445fe997974f
조성원 <tjddnjsjo@kakao.com>
2023-12-11 08:26:13 +0900

    refactor: 프로젝트 구조 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from 'next'
 
 import ThemeProvider from '@/components/ui/theme-provider'
-import Header from '@/components/Header/Header'
-import Footer from '@/components/Footer/Footer'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 import '@/styles/globals.css'
 
 export const metadata: Metadata = {
