9dd459e72564a5a38aba23ba775c747f273622d7
조성원 <tjddnjsjo@kakao.com>
2023-12-11 08:23:21 +0900

    feat: Footer 구현

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <div className="container">
-        <span className="text-muted">Place sticky footer content here.</span>
-      </div>
-    </footer>
-  )
-}
