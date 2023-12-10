064c4260a7514c22359f5cdd9f48ef12f6164158
조성원 <tjddnjsjo@kakao.com>
2023-12-11 06:56:14 +0900

    chore: 프로젝트 구조 변경

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="header">
-      <div className="container">
-        <h1 className="header__title">React Todo</h1>
-      </div>
-    </header>
-  )
-}
