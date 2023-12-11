80c8e0df8ed3b19db44eda1d91928d47e3474253
조성원 <tjddnjsjo@kakao.com>
2023-12-12 05:19:00 +0900

    chore: Contentlayer 설정

---
+++ contentlayer.config.ts
@@ -0,0 +1,22 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+export const Article = defineDocumentType(() => ({
+  name: 'Article',
+  filePathPattern: `**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (article) => `/article/${article._raw.flattenedPath}`,
+    },
+  },
+}))
+
+export default makeSource({
+  contentDirPath: 'articles',
+  documentTypes: [Article],
+})
