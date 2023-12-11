deb45e554a5cfb3cec0a86e13111449e899246df
조성원 <tjddnjsjo@kakao.com>
2023-12-12 04:30:50 +0900

    feat: 레이아웃 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
+
+import { cn } from '@/lib/utils'
 import { ThemeProvider } from '@/components/theme-provider'
+import { SiteHeader } from '@/components/site-header'
+import { SiteFooter } from '@/components/site-footer'
 
 const wanted = localFont({
   src: '../public/fonts/WantedSansVariable.woff2',
@@ -24,14 +28,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className={`${wanted.variable} font-wanted antialiased`}>
+      <body
+        className={cn(
+          'font-wanted min-h-screen bg-background antialiased',
+          wanted.variable,
+        )}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <div className="relative flex min-h-screen flex-col">
+            <SiteHeader />
+            <div className="flex-1">{children}</div>
+            <SiteFooter />
+          </div>
         </ThemeProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return 'Hello, World!'
+  return <div className="container relative">Hello, World!</div>
 }
