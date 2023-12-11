514b1c903d3cae48dfddedbee70b01be5294514a
조성원 <tjddnjsjo@kakao.com>
2023-12-12 04:30:23 +0900

    feat: SiteHeader 컴포넌트 추가

---
+++ components/main-nav.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { cn } from '@/lib/utils'
+
+export function MainNav() {
+  const pathname = usePathname()
+
+  return (
+    <div className="mr-4 hidden md:flex">
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+        {/* <Icons.logo className="h-6 w-6" /> */}
+        <span className="hidden sm:inline-block">
+          <strong>won</strong>tech
+        </span>
+      </Link>
+      <nav className="flex items-center space-x-6 text-sm font-medium">
+        <Link
+          href="/article"
+          className={cn(
+            'transition-colors hover:text-foreground/80',
+            pathname?.startsWith('/article')
+              ? 'text-foreground'
+              : 'text-foreground/60',
+          )}
+        >
+          블로그
+        </Link>
+        <Link
+          href="/project"
+          className={cn(
+            'transition-colors hover:text-foreground/80',
+            pathname?.startsWith('/project')
+              ? 'text-foreground'
+              : 'text-foreground/60',
+          )}
+        >
+          프로젝트
+        </Link>
+      </nav>
+    </div>
+  )
+}
