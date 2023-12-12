a437836837e0365fd591c7d42cd56457273a99bf
조성원 <tjddnjsjo@kakao.com>
2023-12-12 15:57:40 +0900

    feat: 블로그 페이지 구현

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,38 +1,20 @@
-import Link from 'next/link'
-import { compareDesc, format, parseISO } from 'date-fns'
-import { allArticles, Article } from 'contentlayer/generated'
-
-function PostCard(post: Article) {
-  return (
-    <div className="mb-8">
-      <h2 className="mb-1 text-xl">
-        <Link
-          href={post.url}
-          className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
-        >
-          {post.title}
-        </Link>
-      </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-        {format(parseISO(post.date), 'LLLL d, yyyy')}
-      </time>
-    </div>
-  )
-}
+import { compareDesc } from 'date-fns'
+import { allArticles } from 'contentlayer/generated'
+import { ArticleCard } from '@/containers/article/ArticleCard'
 
 export default function Home() {
-  const posts = allArticles.sort((a, b) =>
+  const articles = allArticles.sort((a, b) =>
     compareDesc(new Date(a.date), new Date(b.date)),
   )
 
   return (
-    <div className="mx-auto max-w-xl py-8">
-      <h1 className="mb-8 text-center text-2xl font-black">
-        Next.js + Contentlayer Example
-      </h1>
-      {posts.map((post, idx) => (
-        <PostCard key={idx} {...post} />
-      ))}
+    <div className="container">
+      <h1 className="mb-8 text-2xl font-medium">블로그</h1>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {articles.map((article, idx) => (
+          <ArticleCard key={idx} {...article} />
+        ))}
+      </div>
     </div>
   )
 }
