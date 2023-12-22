a145def0c268b75d66091cd78b6faa2831a96f29
조성원 <tjddnjsjo@kakao.com>
2023-12-22 10:34:36 +0900

    feat: TOC 스크롤 헤더 강조 효과 추가

--- src/containers/article/TableOfContents.tsx
+++ src/containers/article/TableOfContents.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import Link from 'next/link'
 import { cn } from '@/libs/utils'
 import { Separator } from '@/components/ui/separator'
+import { useTocScroll } from '@/libs/useTocScroll'
 
 // need to refactor: any type, tailwind classes
 export function TableOfContents({
@@ -10,25 +13,26 @@ export function TableOfContents({
   article: any
   slug: string
 }) {
+  const { activeToc, headingTops } = useTocScroll(article?.headings)
+
   return (
     <div className="sticky top-24 rounded-md border p-4">
       <h3 className="mb-4 text-sm font-extrabold leading-none">On this page</h3>
       <Separator className="my-2" />
-      {article.headings.map(
-        (heading: { slug: string; level: number; text: string }) => (
-          <div key={`#${heading.slug}`} className="w-64 truncate">
-            <Link
-              className={cn(
-                'text-sm hover:font-bold hover:text-primary',
-                heading.level - 2 && 'ml-4',
-              )}
-              href={`${slug}#${heading.slug}`}
-            >
-              {heading.text}
-            </Link>
-          </div>
-        ),
-      )}
+      {headingTops?.map((heading) => (
+        <div key={`#${heading.slug}`} className="w-64 truncate">
+          <Link
+            className={cn(
+              'text-sm hover:font-bold hover:text-primary',
+              heading.level - 2 && 'ml-4',
+              activeToc === heading.slug && 'font-bold text-primary',
+            )}
+            href={`${slug}#${heading.slug}`}
+          >
+            {heading.text}
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
