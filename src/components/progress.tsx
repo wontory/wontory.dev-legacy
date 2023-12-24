d6c87dc8e7fff2ae3411fc7c5d60f716ae8890ad
조성원 <tjddnjsjo@kakao.com>
2023-12-24 10:12:10 +0900

    style: Progress 스타일 수정

--- src/components/progress.tsx
+++ src/components/progress.tsx
@@ -6,9 +6,11 @@ export function Progress() {
   const { scrollYProgress } = useScroll()
 
   return (
-    <motion.div
-      className="sticky bottom-0 h-2 w-full origin-left bg-primary"
-      style={{ scaleX: scrollYProgress }}
-    />
+    <div className="sticky bottom-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <motion.div
+        className="h-2 w-full origin-left bg-primary"
+        style={{ scaleX: scrollYProgress }}
+      />
+    </div>
   )
 }
