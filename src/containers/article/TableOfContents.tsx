81771680b46d5ff28995a5516ab5df4f5f6d0555
조성원 <tjddnjsjo@kakao.com>
2023-12-26 18:29:42 +0900

    fix: TOC 반응형 수정

--- src/containers/article/TableOfContents.tsx
+++ src/containers/article/TableOfContents.tsx
@@ -19,20 +19,25 @@ export function TableOfContents({
     <div className="sticky top-24 rounded-md border p-4">
       <h3 className="mb-4 text-sm font-extrabold leading-none">On this page</h3>
       <Separator className="my-2" />
-      {headingTops?.map((heading) => (
-        <div key={`#${heading.slug}`} className="w-64 truncate">
-          <Link
-            className={cn(
-              'text-sm hover:font-bold hover:text-primary',
-              heading.level - 2 && 'ml-4',
-              activeToc === heading.slug && 'font-bold text-primary',
-            )}
-            href={`${slug}#${heading.slug}`}
-          >
-            {heading.text}
-          </Link>
-        </div>
-      ))}
+      <div
+        className="overflow-scroll"
+        style={{ maxHeight: 'calc(100vh - 196px)' }}
+      >
+        {headingTops?.map((heading) => (
+          <div key={`#${heading.slug}`} className="w-64 truncate">
+            <Link
+              className={cn(
+                'text-sm hover:font-bold hover:text-primary',
+                heading.level - 2 && 'ml-4',
+                activeToc === heading.slug && 'font-bold text-primary',
+              )}
+              href={`${slug}#${heading.slug}`}
+            >
+              {heading.text}
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
