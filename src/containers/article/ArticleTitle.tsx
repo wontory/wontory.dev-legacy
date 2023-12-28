0ee35381631cdbe2daea4284c655bc417203c9ee
조성원 <tjddnjsjo@kakao.com>
2023-12-28 09:19:01 +0900

    feat: ArticleTitle 저자 추가

--- src/containers/article/ArticleTitle.tsx
+++ src/containers/article/ArticleTitle.tsx
@@ -2,18 +2,31 @@ import { format, parseISO } from 'date-fns'
 
 import { Badge } from '@/components/ui/badge'
 import { Article } from 'contentlayer/generated'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 export function ArticleTitle({ article }: { article: Article }) {
   return (
     <>
       <Badge className="mb-4 text-sm">{article.category}</Badge>
-      <h1 className="mb-4 text-4xl font-bold">{article.title}</h1>
-      <time
-        dateTime={article.date}
-        className="mb-1 text-xs text-gray-600 dark:text-gray-400"
-      >
-        {format(parseISO(article.date), 'LLLL d, yyyy')}
-      </time>
+      <h1 className="mb-8 text-4xl font-bold">{article.title}</h1>
+      <div className="flex items-center gap-4">
+        <Avatar className="h-12 w-12">
+          <AvatarImage
+            src="https://avatars.githubusercontent.com/u/94912717?v=4"
+            alt="@wontory"
+          />
+          <AvatarFallback>W</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col">
+          <h3 className="font-medium">@wontory</h3>
+          <time
+            dateTime={article.date}
+            className="mb-1 text-xs text-gray-600 dark:text-gray-400"
+          >
+            Frontend Developer · {format(parseISO(article.date), 'LLL d, yyyy')}
+          </time>
+        </div>
+      </div>
     </>
   )
 }
