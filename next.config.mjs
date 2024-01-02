3ca1b9f97735f8f2c8bd7e5def5101a0e35c3361
조성원 <tjddnjsjo@kakao.com>
2024-01-02 18:26:56 +0900

    chore: Install @next/mdx

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+}
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+  },
+})
+
+export default withMDX(nextConfig)
