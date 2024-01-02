9c4397ae9e3bce51838d771e784c80e588904f4b
조성원 <tjddnjsjo@kakao.com>
2024-01-02 20:45:06 +0900

    design: 최대 너비 지정

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
         제작중인 블로그입니다.
         <br /> 안녕하세요. 프론트엔드 개발자 wontory입니다.
       </div>
-      <div className="container relative">
+      <div className="container relative max-w-screen-xl">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
           {DUMMY_DATA.map((article) => (
             <Article
