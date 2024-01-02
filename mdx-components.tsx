ffcc5d67fe5d47651db2629608539b57ec4f022c
조성원 <tjddnjsjo@kakao.com>
2024-01-02 18:27:49 +0900

    feat: Add Typography for MDX

---
+++ mdx-components.tsx
@@ -0,0 +1,78 @@
+import type { MDXComponents } from 'mdx/types'
+import Image, { ImageProps } from 'next/image'
+import Link from 'next/link'
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    h1: ({ children }) => (
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        {children}
+      </h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        {children}
+      </h2>
+    ),
+    h3: ({ children }) => (
+      <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
+        {children}
+      </h3>
+    ),
+    h4: ({ children }) => (
+      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+        {children}
+      </h4>
+    ),
+    p: ({ children }) => (
+      <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>
+    ),
+    blockquote: ({ children }) => (
+      <blockquote className="mt-6 border-l-2 pl-6 italic">
+        {children}
+      </blockquote>
+    ),
+    table: ({ children }) => (
+      <div className="my-6 w-full overflow-y-auto">
+        <table className="w-full">{children}</table>
+      </div>
+    ),
+    tr: ({ children }) => (
+      <tr className="m-0 border-t p-0 even:bg-muted">{children}</tr>
+    ),
+    th: ({ children }) => (
+      <th className="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right">
+        {children}
+      </th>
+    ),
+    td: ({ children }) => (
+      <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+        {children}
+      </td>
+    ),
+    ul: ({ children }) => (
+      <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>
+    ),
+    code: ({ children }) => (
+      <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+        {children}
+      </code>
+    ),
+    img: (props) => (
+      <Image
+        sizes="100vw"
+        style={{ width: '100%', height: 'auto' }}
+        {...(props as ImageProps)}
+      />
+    ),
+    a: ({ children, href }) => (
+      <Link
+        href={href as string}
+        className="font-medium text-primary underline underline-offset-4"
+      >
+        {children}
+      </Link>
+    ),
+    ...components,
+  }
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    './mdx-components.tsx',
   ],
   prefix: '',
   theme: {
