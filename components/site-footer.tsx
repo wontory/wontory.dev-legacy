7ae5daa87a54d0b54e9ffc62c9d2109d42b2562f
조성원 <tjddnjsjo@kakao.com>
2024-01-02 19:25:05 +0900

    feat: Add SiteFooter

---
+++ components/site-footer.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+export function SiteFooter() {
+  return (
+    <footer className="py-6 md:px-8 md:py-0">
+      <div className="container flex items-center justify-center md:h-24">
+        <p className="text-sm text-muted-foreground">
+          © 2024{' '}
+          <Link
+            href="https://github.com/wontory"
+            className="font-medium underline underline-offset-4 duration-200 hover:text-primary"
+          >
+            wontory
+          </Link>
+          . All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
