4eed9d42eb36e2f8dda63ea518cacb84a33d01ea
조성원 <tjddnjsjo@kakao.com>
2024-01-03 17:21:01 +0900

    chore: Contentlayer 설정 추가

---
+++ contentlayer.config.ts
@@ -0,0 +1,39 @@
+import {
+  ComputedFields,
+  defineDocumentType,
+  makeSource,
+} from 'contentlayer/source-files'
+import remarkGfm from 'remark-gfm'
+
+const computedFields: ComputedFields = {
+  slug: {
+    type: 'string',
+    resolve: (doc) => `/${doc._raw.flattenedPath}`,
+  },
+  slugAsParams: {
+    type: 'string',
+    resolve: (doc) => doc._raw.flattenedPath.split('/').slice(1).join('/'),
+  },
+}
+
+export const Article = defineDocumentType(() => ({
+  name: 'Article',
+  filePathPattern: `articles/**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    description: { type: 'string', required: true },
+    category: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+  },
+  computedFields,
+}))
+
+export default makeSource({
+  contentDirPath: 'contents',
+  documentTypes: [Article],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
