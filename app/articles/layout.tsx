657a1e71006960dfd26ba73572a03e63999634be
조성원 <tjddnjsjo@kakao.com>
2024-01-03 15:58:32 +0900

    design: 본문 영역 padding-y 추가

--- app/articles/layout.tsx
+++ app/articles/layout.tsx
@@ -1,3 +1,5 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
-  return <div className="container relative max-w-screen-md">{children}</div>
+  return (
+    <div className="container relative max-w-screen-md py-10">{children}</div>
+  )
 }
