86be6eb4a26b8c8b6be2f629be609e9bbf2a3cc3
조성원 <tjddnjsjo@kakao.com>
2024-01-09 01:06:57 +0900

    refactor: badgeVariants 분리

--- components/article-card.tsx
+++ components/article-card.tsx
@@ -3,16 +3,7 @@ import { format, parseISO } from 'date-fns'
 import { Badge } from '@/components/ui/badge'
 import { cn } from '@/libs/utils'
 import { Article } from '@/.contentlayer/generated'
-
-const badgeVariants: { [category: string]: string } = {
-  javascript:
-    'border-transparent bg-[#F7DF1E] text-black hover:bg-[#F7DF1E]/80',
-  typescript:
-    'border-transparent bg-[#3178C6] text-white hover:bg-[#3178C6]/80',
-  react: 'border-transparent bg-[#61DAFB] text-black hover:bg-[#61DAFB]/80',
-  'next.js': 'border-transparent bg-[#000000] text-white hover:bg-[#000000]/80',
-  aws: 'border-transparent bg-[#232F3E] text-white hover:bg-[#232F3E]/80',
-}
+import { badgeVariants } from '@/styles/badgeVariants'
 
 export function ArticleCard(article: Article) {
   return (
---
+++ styles/badgeVariants.ts
@@ -0,0 +1,9 @@
+export const badgeVariants: { [category: string]: string } = {
+  javascript:
+    'border-transparent bg-[#F7DF1E] text-black hover:bg-[#F7DF1E]/80',
+  typescript:
+    'border-transparent bg-[#3178C6] text-white hover:bg-[#3178C6]/80',
+  react: 'border-transparent bg-[#61DAFB] text-black hover:bg-[#61DAFB]/80',
+  'next.js': 'border-transparent bg-[#000000] text-white hover:bg-[#000000]/80',
+  aws: 'border-transparent bg-[#232F3E] text-white hover:bg-[#232F3E]/80',
+}
