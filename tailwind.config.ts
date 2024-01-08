0324fe965e2be03ee06f103443bc6b9b067b9a6d
조성원 <tjddnjsjo@kakao.com>
2024-01-09 01:33:45 +0900

    fix: styles 폴더 TailwindCSS 미적용 오류 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-    './mdx-components.tsx',
+    './styles/**/*.{ts,tsx}',
   ],
   prefix: '',
   theme: {
