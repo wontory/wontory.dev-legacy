3fcd1c20d14b26f2dba8540403098c7974e75986
조성원 <tjddnjsjo@kakao.com>
2024-01-24 22:09:18 +0900

    feat: 페이지 전환 애니메이션 추가

---
+++ app/template.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import { motion, AnimatePresence } from 'framer-motion'
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  )
+}
