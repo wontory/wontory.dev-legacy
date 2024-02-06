283f8ea738db4f0db8ed9b2d68d9581fe9602fa1
조성원 <tjddnjsjo@kakao.com>
2024-02-06 19:01:13 +0900

    feat: date format 로직 변경

--- components/article-info.tsx
+++ components/article-info.tsx
@@ -1,4 +1,10 @@
-import { formatRelative, parseISO } from 'date-fns'
+import {
+  differenceInDays,
+  format,
+  formatDistanceToNow,
+  formatRelative,
+  parseISO,
+} from 'date-fns'
 import { ko } from 'date-fns/locale'
 import { CalendarIcon, StopwatchIcon } from '@radix-ui/react-icons'
 
@@ -7,13 +13,15 @@ import type { Article } from '@/.contentlayer/generated'
 type ArticleInfoProps = Pick<Article, 'date' | 'readingTime'>
 
 export function ArticleInfo({ date, readingTime }: ArticleInfoProps) {
-  const formatDate = (today: Date, date: Date) =>
-    formatRelative(date, today, { locale: ko })
+  const formatDate = (date: Date) =>
+    differenceInDays(new Date(), date) > 2
+      ? format(date, 'yyyy.MM.dd')
+      : formatDistanceToNow(date, { addSuffix: true, locale: ko })
 
   return (
     <div className="flex gap-2 text-xs text-muted-foreground">
       <CalendarIcon />
-      <time dateTime={date}>{formatDate(new Date(), parseISO(date))}</time>
+      <time dateTime={date}>{formatDate(parseISO(date))}</time>
       &nbsp;
       <StopwatchIcon />
       <span>{readingTime}분</span>
