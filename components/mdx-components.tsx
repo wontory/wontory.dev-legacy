3c55412d7ca0466e3a05ef5e1b94d6fd676cb60e
조성원 <tjddnjsjo@kakao.com>
2024-02-15 19:12:29 +0900

    design: inline code 글씨 굵기 수정

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -81,7 +81,7 @@ const components: MDXComponents = {
   code: ({ className, ...props }) => (
     <code
       className={cn(
-        'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold',
+        'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm',
         className,
       )}
       {...props}
