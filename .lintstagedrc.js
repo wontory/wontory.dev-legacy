a4b4a611169cedefe694351f91f4063137ef7b3f
조성원 <tjddnjsjo@kakao.com>
2024-02-22 19:38:32 +0900

    feat: initial commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx,md,html,css}': [buildPrettierCommand],
+}
