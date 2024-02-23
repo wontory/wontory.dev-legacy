14f28aad5b81aef6a97bba6d49fc07292673477d
조성원 <tjddnjsjo@kakao.com>
2024-02-23 21:50:37 +0900

    chore: install rehype-pretty-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+import rehypePrettyCode from 'rehype-pretty-code'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'dracula' },
+      syntaxHighlight: false,
+      rehypePlugins: [rehypePrettyCode],
       gfm: true,
     }),
     react(),
