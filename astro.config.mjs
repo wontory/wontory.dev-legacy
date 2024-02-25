1b43770e352313dbac0d9512be34711ad7023f84
조성원 <tjddnjsjo@kakao.com>
2024-02-25 16:54:34 +0900

    feat: astro config site 주소 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://www.wontory.dev',
   integrations: [
     mdx(),
     react(),
