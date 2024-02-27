2f6037bbbc5726462d3049840f1d861afd55afaf
조성원 <tjddnjsjo@kakao.com>
2024-02-27 22:43:05 +0900

    feat: 이미지 CLS 개선

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,13 +2,14 @@ import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image(),
+    }),
 })
 
 export const collections = { blog }
