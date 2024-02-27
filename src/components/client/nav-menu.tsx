4abbe2718bf60b5cf88bdd8f3ef0c882a43c99fd
조성원 <tjddnjsjo@kakao.com>
2024-02-28 04:52:10 +0900

    refactor: subpath 로직 위치 변경

--- src/components/client/nav-menu.tsx
+++ src/components/client/nav-menu.tsx
@@ -7,12 +7,14 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 
-export function NavMenu({ path }: { path: string | undefined }) {
+export function NavMenu({ url }: { url: URL }) {
+  const subpath = url.pathname.split('/').filter(Boolean)
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="text-md group capitalize">
-          {path}
+          {subpath[0]}
           <ChevronDown className="ml-2 h-4 w-4 transition-transform group-data-[state=open]:rotate-180" />
         </Button>
       </DropdownMenuTrigger>
