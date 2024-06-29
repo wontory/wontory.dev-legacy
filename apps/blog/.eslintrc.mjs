22cf947c5910efd3a0f87176bb3ca88287d2e915
조성원 <tjddnjsjo@kakao.com>
2024-06-29 21:21:49 +0900

    style: format code

--- apps/blog/.eslintrc.mjs
+++ apps/blog/.eslintrc.mjs
@@ -1,11 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   root: true,
-  extends: ["@wontory/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@wontory/eslint-config/next.js'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
-};
+}
 
-export default config;
+export default config
--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,10 +1,10 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@wontory/eslint-config/react-internal.js"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@wontory/eslint-config/react-internal.js'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "./tsconfig.lint.json",
+    project: './tsconfig.lint.json',
     tsconfigRootDir: __dirname,
   },
-};
+}
