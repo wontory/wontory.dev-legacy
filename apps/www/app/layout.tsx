a1e357f1c53c177319e7cd66d61badd15c5cd5be
조성원 <tjddnjsjo@kakao.com>
2024-06-29 20:16:27 +0900

    chore(www): create www app

---
+++ apps/www/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '@wontory/tailwindcss-config/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'www.wontory.dev',
+  description: 'www.wontory.dev',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ apps/www/tailwind.config.ts
@@ -0,0 +1,1 @@
+module.exports = require('@wontory/tailwindcss-config/tailwind.config')
