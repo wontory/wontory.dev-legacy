61d12c2f39cfbc924e99088ae7ac7dfc24a0c767
조성원 <tjddnjsjo@kakao.com>
2024-06-30 16:05:20 +0900

    chore(ui): update package exports and add global styles

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -3,6 +3,17 @@ import base from '@wontory/tailwindcss-config/tailwind.config'
 
 const config = {
   ...base,
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+      },
+      boxShadow: {
+        button:
+          '0 0 0 2px rgba(0,0,0,.5),0 0 14px 0 hsla(0,0%,100%,.19),inset 0 -1px .4px 0 rgba(0,0,0,.2),inset 0 1px .4px 0 #fff',
+      },
+    },
+  },
 } satisfies Config
 
 export default config
