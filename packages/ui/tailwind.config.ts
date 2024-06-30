957251bf4d4c499ed471d3823b69ea88a2cd6c24
조성원 <tjddnjsjo@kakao.com>
2024-06-30 23:29:08 +0900

    feat(ui): add font support for SUIT font

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -5,12 +5,9 @@ const config = {
   ...base,
   theme: {
     extend: {
+      fontFamily: { sans: ['var(--font-suit)'] },
       colors: {
         background: 'var(--background)',
-      },
-      boxShadow: {
-        button:
-          '0 0 0 2px rgba(0,0,0,.5),0 0 14px 0 hsla(0,0%,100%,.19),inset 0 -1px .4px 0 rgba(0,0,0,.2),inset 0 1px .4px 0 #fff',
       },
     },
   },
