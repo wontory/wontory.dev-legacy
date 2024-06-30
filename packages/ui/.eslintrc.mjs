67eab384bd5d94db70dce466bbfec35432be2edf
조성원 <tjddnjsjo@kakao.com>
2024-06-30 11:55:52 +0900

    chore(ui): update eslint configuration to use .mjs file format and remove tsconfig.lint.json

--- packages/ui/.eslintrc.mjs
+++ packages/ui/.eslintrc.mjs
@@ -1,10 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
-module.exports = {
+const config = {
   root: true,
   extends: ['@wontory/eslint-config/react-internal.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.lint.json',
-    tsconfigRootDir: __dirname,
+    project: true,
   },
 }
+
+export default config
