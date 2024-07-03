9c7bb1775628a18e11c9d1440e95eafd502e81b9
조성원 <tjddnjsjo@kakao.com>
2024-07-03 18:53:22 +0900

    feat(blog): create post page for content view

---
+++ apps/blog/app/post/[slug]/page.tsx
@@ -0,0 +1,19 @@
+import { getPost } from '~/queries/hashnode'
+import { PostInfo } from '~/components/post-info'
+
+export default async function Post({ params }: { params: { slug: string } }) {
+  const post = await getPost(params.slug)
+
+  return (
+    <div className="flex flex-col items-center gap-12">
+      <PostInfo post={post} mode="header" className="max-w-screen-md" />
+      <hr className="my-3 w-full" />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: post.content.html,
+        }}
+        className="prose text-muted-foreground max-w-screen-md text-xl font-light"
+      />
+    </div>
+  )
+}
