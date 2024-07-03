17421d140e895942f0e37431796a76c7a6aa896e
조성원 <tjddnjsjo@kakao.com>
2024-07-03 16:13:05 +0900

    refactor(blog): separate `PostCard` component from `page.tsx`

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -1,48 +1,19 @@
-import Image from 'next/image'
-import { format } from 'date-fns'
+import Link from 'next/link'
 
 import { getAllPosts } from '~/queries/hashnode'
+import { PostCard } from '~/components/post-card'
 
 export default async function Blog() {
   const posts = await getAllPosts()
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-12">
       <h1 className="text-5xl font-semibold">Blog</h1>
       <div className="grid w-full gap-8 md:grid-cols-2">
         {posts.map((post) => (
-          <div
-            key={post.id}
-            className="bg-background hover:bg-foreground/5 hover:border-border border-border/80 flex flex-col overflow-hidden rounded-md border transition-colors duration-200"
-          >
-            <div className="relative aspect-video">
-              <Image
-                src={post.coverImage.url}
-                alt="test"
-                draggable="false"
-                className="object-cover"
-                fill
-              />
-            </div>
-            <div className="flex flex-col gap-4 p-8">
-              <h2 className="text-lg font-semibold">{post.title}</h2>
-              <p className="text-muted-foreground line-clamp-2">{post.brief}</p>
-              <div className="mt-4 grid grid-cols-2">
-                <div className="flex flex-col gap-1.5">
-                  <span className="text-muted-foreground/70 text-sm">
-                    Written by
-                  </span>
-                  <div>wontory</div>
-                </div>
-                <div className="flex flex-col gap-1.5">
-                  <span className="text-muted-foreground/70 text-sm">
-                    Published on
-                  </span>
-                  <div>{format(post.publishedAt, 'PPP')}</div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Link key={post.id} href={`/post/${post.slug}`}>
+            <PostCard post={post} />
+          </Link>
         ))}
       </div>
     </div>
---
+++ apps/blog/src/components/post-card.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import { format } from 'date-fns'
+
+import type { Post } from '~/types/post'
+
+function PostCard({ post }: { post: Post }) {
+  return (
+    <div className="bg-background hover:bg-foreground/5 hover:border-border border-border/80 flex flex-col overflow-hidden rounded-md border transition-colors duration-200">
+      <div className="relative aspect-video">
+        <Image
+          src={post.coverImage.url}
+          alt="test"
+          draggable="false"
+          className="object-cover"
+          fill
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-8">
+        <h2 className="text-lg font-semibold">{post.title}</h2>
+        <p className="text-muted-foreground line-clamp-2">{post.brief}</p>
+        <div className="mt-4 grid grid-cols-2">
+          <div className="flex flex-col gap-1.5">
+            <span className="text-muted-foreground/70 text-sm">Written by</span>
+            <div>wontory</div>
+          </div>
+          <div className="flex flex-col gap-1.5">
+            <span className="text-muted-foreground/70 text-sm">
+              Published on
+            </span>
+            <div>{format(post.publishedAt, 'PPP')}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export { PostCard }
