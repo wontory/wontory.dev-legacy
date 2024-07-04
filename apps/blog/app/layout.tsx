fd1d3f0b0a4388779baee75233f52f511dc0cc1a
조성원 <tjddnjsjo@kakao.com>
2024-07-04 15:59:07 +0900

    feat(ui): add `ThemeProvider` component for theming support

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { cn } from '@wontory/util/cn'
 import { SiteHeader } from '@wontory/ui/site-header'
 import { SiteFooter } from '@wontory/ui/site-footer'
+import { ThemeProvider } from '@wontory/ui/theme-provider'
 import { Pretendard } from '@wontory/ui/font/pretendard'
 import '@wontory/ui/globals.css'
 
@@ -16,15 +17,22 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ko">
-      <body className={cn(Pretendard.variable, 'dark font-sans')}>
-        <div className="relative flex min-h-screen flex-col">
-          <SiteHeader />
-          <main className="container max-w-screen-lg flex-1 px-6 py-40">
-            {children}
-          </main>
-          <SiteFooter />
-        </div>
+    <html lang="ko" suppressHydrationWarning>
+      <body className={cn(Pretendard.variable, 'font-sans')}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="relative flex min-h-screen flex-col">
+            <SiteHeader />
+            <main className="container max-w-screen-lg flex-1 px-6 py-40">
+              {children}
+            </main>
+            <SiteFooter />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   )
