8c9b389f63608d7ed998765478c04a0438c5e5c7
조성원 <tjddnjsjo@kakao.com>
2024-07-05 00:15:53 +0900

    chore(blog): add `@tailwindcss/typography` plugin to tailwind.config.ts

--- apps/blog/tailwind.config.ts
+++ apps/blog/tailwind.config.ts
@@ -1,1 +1,9 @@
-export { default } from '@wontory/ui/tailwind.config.ts'
+import type { Config } from 'tailwindcss'
+import base from '@wontory/ui/tailwind.config.ts'
+
+const config = {
+  ...base,
+  plugins: [require('@tailwindcss/typography')],
+} satisfies Config
+
+export default config
