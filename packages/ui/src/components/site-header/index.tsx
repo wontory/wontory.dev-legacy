81c658e8ca7489b6bf86e1d3933b4cf6160215e7
조성원 <tjddnjsjo@kakao.com>
2024-07-04 22:41:29 +0900

    refactor(ui): update site header links to use environment variables

--- packages/ui/src/components/site-header/index.tsx
+++ packages/ui/src/components/site-header/index.tsx
@@ -6,11 +6,13 @@ export function SiteHeader() {
       <div className="bg-background/90 rounded-lg backdrop-blur-sm">
         <div className="from-foreground/[0.03] to-foreground/[0.01] shadow-foreground/15 border-border/60 shadow-header flex w-full items-center rounded-lg border bg-gradient-to-tr p-4 md:px-8 md:py-4 [&>*]:items-center">
           <div className="inline-flex w-1/2 justify-start [&>a]:text-lg [&>a]:font-medium">
-            <Link href="https://www.wontory.dev">wontory.dev</Link>
+            <Link href={process.env.NEXT_PUBLIC_WWW_URL as string}>
+              wontory.dev
+            </Link>
           </div>
           <div className="[&>a]:text-muted-foreground/90 [&>a:hover]:text-foreground hidden shrink-0 gap-4 md:inline-flex [&>a]:px-2 [&>a]:py-3 [&>a]:text-sm [&>a]:font-medium [&>a]:transition-colors [&>a]:duration-200">
-            <Link href="https://www.wontory.dev">Home</Link>
-            <Link href="https://blog.wontory.dev">Blog</Link>
+            <Link href={process.env.NEXT_PUBLIC_WWW_URL as string}>Home</Link>
+            <Link href={process.env.NEXT_PUBLIC_BLOG_URL as string}>Blog</Link>
           </div>
           <div className="inline-flex w-1/2 justify-end">
             <button className="shadow-button text-background hidden rounded-sm bg-neutral-200 px-3 py-2 text-sm font-medium transition-colors duration-200 hover:bg-white md:block">
