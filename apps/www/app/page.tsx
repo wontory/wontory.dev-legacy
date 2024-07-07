974a6a66608ff750911068e2338401f3f6c5bfb0
조성원 <tjddnjsjo@kakao.com>
2024-07-08 00:55:43 +0900

    refactor(www): update home page

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="w-full">Hello, home!</div>
+    <div className="flex flex-col gap-12">
+      <h1 className="text-5xl font-semibold">Home</h1>
     </div>
   )
 }
