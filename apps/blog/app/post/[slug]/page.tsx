d57c84a871ac042e827872173af84fb94501124e
조성원 <tjddnjsjo@kakao.com>
2024-07-08 00:23:32 +0900

    refactor(blog): update text color for better accessibility

--- apps/blog/app/post/[slug]/page.tsx
+++ apps/blog/app/post/[slug]/page.tsx
@@ -12,7 +12,7 @@ export default async function Post({ params }: { params: { slug: string } }) {
         dangerouslySetInnerHTML={{
           __html: post.content.html,
         }}
-        className="prose text-muted-foreground selection:text-muted max-w-screen-md text-xl font-light leading-relaxed selection:bg-white"
+        className="prose text-foreground/70 selection:text-muted max-w-screen-md text-xl font-light leading-relaxed selection:bg-white"
       />
     </div>
   )
