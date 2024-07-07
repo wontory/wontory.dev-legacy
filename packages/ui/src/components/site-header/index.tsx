aeaf9b6aaa2a57edff9cbcc2e07f58b358ba7a6f
조성원 <tjddnjsjo@kakao.com>
2024-07-07 11:54:10 +0900

    chore(ui): update site header styles

--- packages/ui/src/components/site-header/index.tsx
+++ packages/ui/src/components/site-header/index.tsx
@@ -5,8 +5,11 @@ export function SiteHeader() {
     <div className="container fixed left-1/2 top-0 z-50 w-full max-w-screen-lg -translate-x-1/2 px-4 pt-4">
       <div className="bg-background/90 rounded-lg backdrop-blur-sm">
         <div className="from-foreground/[0.03] to-foreground/[0.01] shadow-foreground/15 border-border/60 shadow-header flex w-full items-center rounded-lg border bg-gradient-to-tr p-4 md:px-8 md:py-4 [&>*]:items-center">
-          <div className="inline-flex w-1/2 justify-start [&>a]:text-lg [&>a]:font-medium">
-            <Link href={process.env.NEXT_PUBLIC_WWW_URL as string}>
+          <div className="inline-flex w-1/2 justify-start">
+            <Link
+              href={process.env.NEXT_PUBLIC_WWW_URL as string}
+              className="text-lg font-medium transition-all duration-200 hover:text-[1.17rem]"
+            >
               wontory.dev
             </Link>
           </div>
@@ -15,7 +18,7 @@ export function SiteHeader() {
             <Link href={process.env.NEXT_PUBLIC_BLOG_URL as string}>Blog</Link>
           </div>
           <div className="inline-flex w-1/2 justify-end">
-            <button className="shadow-button text-background hidden rounded-sm bg-neutral-200 px-3 py-2 text-sm font-medium transition-colors duration-200 hover:bg-white md:block">
+            <button className="shadow-button text-background rounded-xs hidden bg-neutral-200 px-3 py-2 text-sm font-medium transition-colors duration-200 hover:bg-white md:block">
               Welcome!
             </button>
           </div>
