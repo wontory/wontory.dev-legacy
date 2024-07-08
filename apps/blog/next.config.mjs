1a3218987e5cd4fabcc2606a2300865627774387
조성원 <tjddnjsjo@kakao.com>
2024-07-08 20:50:47 +0900

    feat: add support for avif and webp formats & `transpilePackages` option

--- apps/blog/next.config.mjs
+++ apps/blog/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['@wontory/ui'],
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
