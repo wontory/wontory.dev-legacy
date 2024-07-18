aa5fd43d6104f28127011cbca8fc4cb6563d66d1
조성원 <tjddnjsjo@kakao.com>
2024-07-18 10:08:56 +0900

    feat: apply next-themes

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { ViewTransitions } from '@wontory/lib/next-view-transitions'
 import { ReactLenis } from '@wontory/lib/lenis'
 import { cn } from '@wontory/util/cn'
+import { ThemeProvider } from '@wontory/ui/theme-provider'
 import { SiteHeader } from '@wontory/ui/site-header'
 import { SiteFooter } from '@wontory/ui/site-footer'
 import { Pretendard } from '@wontory/ui/font/pretendard'
@@ -19,16 +20,23 @@ export default function RootLayout({
 }>) {
   return (
     <ViewTransitions>
-      <html lang="ko">
+      <html lang="ko" suppressHydrationWarning>
         <body className={cn(Pretendard.variable, 'font-sans')}>
-          <div className="relative flex min-h-screen flex-col">
-            <SiteHeader />
-            <main className="container max-w-screen-lg flex-1 px-6 py-40">
-              {children}
-            </main>
-            <SiteFooter />
-          </div>
-          <ReactLenis root />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="relative flex min-h-screen flex-col">
+              <SiteHeader />
+              <main className="container max-w-screen-lg flex-1 px-6 py-40">
+                {children}
+              </main>
+              <SiteFooter />
+            </div>
+            <ReactLenis root />
+          </ThemeProvider>
         </body>
       </html>
     </ViewTransitions>
