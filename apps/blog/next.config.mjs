d6af7ea347efd802418aa8b534622af3d9b0db65
조성원 <tjddnjsjo@kakao.com>
2024-08-09 20:15:14 +0900

    feat: configure velite

--- apps/blog/next.config.mjs
+++ apps/blog/next.config.mjs
@@ -1,14 +1,22 @@
+const isDev = process.argv.indexOf('dev') !== -1
+const isBuild = process.argv.indexOf('build') !== -1
+if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
+  process.env.VELITE_STARTED = '1'
+  const { build } = await import('velite')
+  await build({ watch: isDev, clean: !isDev })
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	transpilePackages: ["@wontory/lib", "@wontory/ui", "next-mdx-remote"],
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "**",
-			},
-		],
-	},
-};
+  transpilePackages: ['@wontory/lib', '@wontory/ui'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
---
+++ apps/blog/velite.config.ts
@@ -0,0 +1,71 @@
+import remarkGfm from 'remark-gfm'
+import { defineCollection, defineConfig, s } from 'velite'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split('/').slice(1).join('/'),
+})
+
+const posts = defineCollection({
+  name: 'Post',
+  pattern: 'posts/**/*.mdx',
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      date: s.isodate(),
+      published: s.boolean().default(true),
+      image: s.string().max(99),
+      authors: s.array(s.string()),
+      toc: s.toc(),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+})
+
+const authors = defineCollection({
+  name: 'Author',
+  pattern: 'authors/**/*.mdx',
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      avatar: s.string().max(99),
+      github: s.string().max(99),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+})
+
+const config = defineConfig({
+  root: 'content',
+  output: {
+    data: '.velite',
+    assets: 'public/static',
+    base: '/static/',
+    name: '[name]-[hash:6].[ext]',
+    clean: true,
+  },
+  collections: { posts, authors },
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ['subheading-anchor'],
+            ariaLabel: 'Link to section',
+          },
+        },
+      ],
+    ],
+  },
+})
+
+export default config
