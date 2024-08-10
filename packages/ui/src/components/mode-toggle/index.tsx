448e29cf6b82c880374316b7ad93cc2a12a2ae3d
조성원 <tjddnjsjo@kakao.com>
2024-08-11 01:21:39 +0900

    chore(ui): add accessibility label to mode toggle button

--- packages/ui/src/components/mode-toggle/index.tsx
+++ packages/ui/src/components/mode-toggle/index.tsx
@@ -19,6 +19,7 @@ export function ModeToggle() {
     >
       <SunIcon className="dark:-rotate-90 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0" />
       <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle Mode</span>
     </button>
   )
 }
