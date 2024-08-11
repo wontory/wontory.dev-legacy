6eb19500fdbd85719506a48e502d25faf79424f4
조성원 <tjddnjsjo@kakao.com>
2024-08-12 06:18:07 +0900

    feat(cv): update page.tsx

--- apps/cv/app/page.tsx
+++ apps/cv/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-12">
       <h1 className="font-semibold text-5xl">CV</h1>
+      <span>Hi, I'm wontory.</span>
     </div>
   )
 }
